1db087b6ac2936dc213c127840ff8c4562066336
Jaime Condori <jaime.condori.machaca@gmail.com>
2026-04-21 19:13:02 -0400

    solucionando problema causado por adapter y pasandon el DATABASE_URL explicitamente

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -30,7 +30,15 @@ const { PrismaClient } = require("@prisma/client")
 // })
 
 // Creamos la instancia de Prisma
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
+
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+})
 
 
 
