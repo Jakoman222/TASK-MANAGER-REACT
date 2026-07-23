363733e15a22ac59e9d5c1cb493a44e699faac92
Jaime Alejandro Condori Machaca <jaime.condori.machaca@gmail.com>
2026-07-22 23:14:12 -0400

    fix: eslint no reconocia ts, por lo tanto se agregaron configuraciones para que lo entienda desde el frontend

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,12 +2,14 @@ import js from '@eslint/js';
 import globals from 'globals';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
+import tseslint from 'typescript-eslint';
 
 export default [
     {
         ignores: ['dist/**', 'coverage/**', 'node_modules/**'],
     },
     js.configs.recommended,
+    ...tseslint.configs.recommended,
     {
         files: ['**/*.{js,jsx,ts,tsx}'],
         languageOptions: {
@@ -30,15 +32,19 @@ export default [
             react: { version: 'detect' },
         },
         rules: {
-            'no-unused-vars': [
+            '@typescript-eslint/no-unused-vars': [
                 'error',
                 { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
             ],
+
+            'no-unused-vars': 'off',
+
             'no-console': ['warn', { allow: ['warn', 'error'] }],
             eqeqeq: ['error', 'always'],
             curly: ['error', 'multi-line'],
             semi: ['error', 'always'],
             quotes: ['error', 'single', { avoidEscape: true }],
+
             'react/react-in-jsx-scope': 'off',
             'react/jsx-uses-react': 'off',
             'react/prop-types': 'off',
