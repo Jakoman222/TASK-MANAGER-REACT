f2b35d321d5bc69ac6148924f81933e1899be993
Jaime Alejandro Condori Machaca <jaime.condori.machaca@gmail.com>
2026-07-27 00:01:50 -0400

    feat: Cambio en el footer para mostrar tareas pendientes y completadas

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,15 +3,12 @@ import './Footer.css';
 type FooterProps = {
     pendingCount: number;
     completedCount: number;
-    onClearCompleted: () => void;
 };
 
 function Footer({
     pendingCount,
     completedCount,
-    onClearCompleted,
 }: FooterProps) {
-    const hasCompletedTasks = completedCount > 0;
 
     return (
         <footer className="task-footer">
@@ -25,14 +22,6 @@ function Footer({
                     {completedCount} completadas
                 </span>
             </div>
-            <button
-                type="button"
-                className="task-footer__button"
-                onClick={onClearCompleted}
-                disabled={!hasCompletedTasks}
-            >
-                Limpiar completadas
-            </button>
         </footer>
     );
 }
